5210f54bcde57768312d55dfdf91bf5fcbd18914
booski <p9346420@gmail.com>
2020-02-16 22:04:21 +0900

    ~#1.3 Apollo Cache and Styles

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,14 +2,50 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+`;
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     get_movie(id: $id) {
-      id
       title
       medium_cover_image
       description_intro
+      language
+      rating
     }
   }
 `;
@@ -21,12 +57,15 @@ export default () => {
     variables: { id }
   });
 
-  if (loading) {
-    return "Loading...";
-  }
-  if (data && data.movie) {
-    return data.movie.title;
-  }
-  console.log(loading, data);
-  return <h1>Detail</h1>;
+  console.log(data);
+  return (
+    <Container>
+      <Column>
+        <Title>Name</Title>
+        <Subtitle>English · 4.5</Subtitle>
+        <Description>lorem ipsum lalalla </Description>
+      </Column>
+      <Poster></Poster>
+    </Container>
+  );
 };
